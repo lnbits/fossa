6c6229d28e37c11c6d8e215f050f49d09148e11f
Arc <ben@arc.wales>
2024-10-10 12:37:24 +0100

    added initialise

--- fossa-tests/testbills/testbills.ino
+++ fossa-tests/testbills/testbills.ino
@@ -6,15 +6,16 @@ HardwareSerial SerialPort1(1);
 void setup() {
   Serial.begin(115200);
   SerialPort1.begin(300, SERIAL_8N2, TX1, RX1);
+  SerialPort1.write(184);
 }
 
 void loop() {
   int x = 0;
   x = SerialPort1.read();
-  if(x > 0){
+  if(x > 0 && x < 10){
     Serial.print("Worked, got: ");
     Serial.print(x);
-    Serial.println(" from bill acceptor");
+    Serial.println(" from bill acceptor (which is the number of the note in the notes list set using the validator software)");
   }
   delay(500);
 }
