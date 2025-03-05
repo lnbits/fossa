1df26bf3736caabcfe64a50ed2d9b434cbd96910
dni ⚡ <office@dnilabs.com>
2025-03-05 12:11:15 +0100

    feat: change to aes-cbc encryption
    more fixes
    
    move forward
    
    fix between

--- fossa/105_display.ino
+++ fossa/105_display.ino
@@ -1,66 +1,101 @@
+#ifdef LAYOUT_1
+    int horizontalOffset = 480;
+    int verticalOffset = 40;
+    int qrPixelSize = 4;
+    int qrHorizontalOffset = 120;
+    int qrVerticalOffset = 20;
+    int qrTextHorizontalOffset = 40;
+    int qrTextVerticalOffset = 290;
+#endif
+#ifdef LAYOUT_2
+    int horizontalOffset = 240;
+    int verticalOffset = 20;
+    int qrPixelSize = 3;
+    int qrHorizontalOffset = 28;
+    int qrVerticalOffset = 28;
+    int qrTextHorizontalOffset = 10;
+    int qrTextVerticalOffset = 220;
+#endif
+
 void printMessage(String text1, String text2, String text3, int ftcolor, int bgcolor) {
   tft.fillScreen(bgcolor);
   tft.setTextColor(ftcolor, bgcolor);
-  tft.setTextSize(4);
-  tft.setCursor((480 - textWidth(text1, 4)) / 2, 40);
+  tft.setTextSize(3);
+  tft.setCursor((horizontalOffset - tft.textWidth(text1)) / 2, verticalOffset);
   tft.println(text1);
-  tft.setCursor((480 - textWidth(text2, 4)) / 2, 120);
+  tft.setCursor((horizontalOffset - tft.textWidth(text2)) / 2, verticalOffset + 60);
   tft.println(text2);
   tft.setTextSize(3);
-  tft.setCursor((480 - textWidth(text3, 3)) / 2, 200);
+  tft.setCursor((horizontalOffset - tft.textWidth(text3)) / 2, verticalOffset + 140);
   tft.println(text3);
 }
 
 void feedmefiat() {
   tft.setTextColor(TFT_WHITE);
-  tft.setCursor((480 - textWidth(fossaT, 2)) / 2, 40);
   tft.setTextSize(2);
+  tft.setCursor((horizontalOffset - tft.textWidth(fossaT)) / 2, verticalOffset);
   tft.println(fossaT);
-  tft.setCursor((480 - textWidth(feedT + " " + String(charge) + chargeT, 2)) / 2, 280);
-  tft.setTextSize(2);
-  tft.println(feedT + " " + String(charge) + chargeT);
+#ifdef LAYOUT_1
+  String printFeedT = feedT + " " + String(charge) + chargeT;
+  tft.setCursor((horizontalOffset - tft.textWidth(printFeedT)) / 2, verticalOffset + 260);
+  tft.println(printFeedT);
+#endif
+#ifdef LAYOUT_2
+  tft.setCursor((horizontalOffset - tft.textWidth(feedT)) / 2, verticalOffset + 240);
+  tft.println(feedT);
+  tft.setCursor((horizontalOffset - tft.textWidth(String(charge) + chargeT)) / 2, verticalOffset + 260);
+  tft.println(String(charge) + chargeT);
+#endif
 }
 
 void feedmefiatloop() {
   tft.setTextColor(homeScreenColors[homeScreenNumColorCount]);
   tft.setTextSize(8);
-  tft.setCursor((480 - textWidth(satsT, 8)) / 2, 80);
+  tft.setCursor((horizontalOffset - tft.textWidth(satsT)) / 2, verticalOffset + 40);
   tft.println(satsT);
-  tft.setCursor((480 - textWidth(forT, 8)) / 2, 140);
+  tft.setCursor((horizontalOffset - tft.textWidth(forT)) / 2, verticalOffset + 100);
   tft.println(forT);
-  tft.setCursor((480 - textWidth(fiatT, 8)) / 2, 200);
+  tft.setCursor((horizontalOffset - tft.textWidth(fiatT)) / 2, verticalOffset + 160);
   tft.println(fiatT);
   delay(100);
 }
 
-void qrShowCodeLNURL(String message) {
+void qrShowCodeLNURL() {
   #ifdef PRINTER
     printMessage(printingT, waitT, "", TFT_WHITE, TFT_BLACK);
     delay(3000);
     printReceipt();
   #endif
-  Serial.println(qrData);
   tft.fillScreen(TFT_WHITE);
   const char *qrDataChar = qrData.c_str();
   QRCode qrcoded;
-  uint8_t qrcodeData[qrcode_getBufferSize(20)];
+  uint8_t qrcodeData[qrcode_getBufferSize(30)];
   qrcode_initText(&qrcoded, qrcodeData, 11, 0, qrDataChar);
 
   for (uint8_t y = 0; y < qrcoded.size; y++) {
     for (uint8_t x = 0; x < qrcoded.size; x++) {
       if (qrcode_getModule(&qrcoded, x, y)) {
-        tft.fillRect(120 + 4 * x, 20 + 4 * y, 4, 4, TFT_BLACK);
+        tft.fillRect(qrHorizontalOffset + qrPixelSize * x, qrVerticalOffset + qrPixelSize * y, qrPixelSize, qrPixelSize, TFT_BLACK);
       } else {
-        tft.fillRect(120 + 4 * x, 20 + 4 * y, 4, 4, TFT_WHITE);
+        tft.fillRect(qrHorizontalOffset + qrPixelSize * x, qrVerticalOffset + qrPixelSize * y, qrPixelSize, qrPixelSize, TFT_WHITE);
       }
     }
   }
-
-  tft.setCursor(40, 290);
-  tft.setTextSize(2);
   tft.setTextColor(TFT_BLACK, TFT_WHITE);
-  tft.println(message);
+  tft.setTextSize(2);
+#ifdef LAYOUT_1
+  tft.setCursor(qrTextHorizontalOffset, qrTextVerticalOffset);
+  tft.println(scanMeT + " " + tapScreenT);
+#endif
+#ifdef LAYOUT_2
+  tft.setCursor(qrTextHorizontalOffset, qrTextVerticalOffset);
+  tft.println(scanMeT);
+  tft.setCursor(qrTextHorizontalOffset, qrTextVerticalOffset + 20);
+  tft.println(tapScreenT);
+#endif
+
   delay(2000);
+
   waitForTap = true;
   while (waitForTap) {
     BTNA.read();
@@ -68,9 +103,4 @@ void qrShowCodeLNURL(String message) {
       waitForTap = false;
     }
   }
-}
-
-int textWidth(String text, int textSize) {
-  tft.setTextSize(textSize);
-  return tft.textWidth(text);
 }
--- fossa/fossa.ino
+++ fossa/fossa.ino
@@ -4,28 +4,36 @@
 #include <SoftwareSerial.h>
 #include <HardwareSerial.h>
 #include <JC_Button.h>
-#include <Hash.h>
 #include <ArduinoJson.h>
 #include "qrcoded.h"
 #include "Bitcoin.h"
-#include <Adafruit_Thermal.h>
-#include <cstdlib>
+#include <TFT_eSPI.h>
+#include "mbedtls/aes.h"
+
 #define FORMAT_ON_FAIL true
 #define PARAM_FILE "/elements.json"
 
-#include <TFT_eSPI.h>
+#define VERSION "0.3.0"
+
+// LAYOUT_1 is horizontal 480x320
+//#define LAYOUT_1
+
+// LAYOUT_2 is vertical 240x320
+#define LAYOUT_2
+
+//#define BILL_ACCEPTOR
+#define BILL_RX 32      // RX Bill acceptor
+#define BILL_TX 33      // TX Bill acceptor
+
+#define COIN_ACCEPTOR
+#define COIN_TX 34      // TX Coinmech
+#define COIN_INHIBIT 35 // Coinmech interrupt
 
 //#define PRINTER
-#define BILL_ACCEPTOR
-#define COIN_ACCEPTOR
+#define PRINTER_RX 22   // RX of the thermal printer
+#define PRINTER_TX 23   // TX of the thermal printer
 
-#define BTN1 39        // Screen tap button
-#define BILL_RX 32     // RX Bill acceptor
-#define BILL_TX 33     // TX Bill acceptor
-#define COIN_TX 4      // TX Coinmech
-#define COIN_INHIBIT 2 // Coinmech
-#define PRINTER_RX 22  // RX of the thermal printer
-#define PRINTER_TX 23  // TX of the thermal printer
+#define BTN1 21         // Screen tap button
 
 // uncomment to use always hardcoded default settings
 #define HARDCODED
@@ -35,7 +43,9 @@
 #define CHARGE 10 // % you will charge people for service, set in LNbits extension
 #define MAX_AMOUNT 30 // max amount per withdraw
 #define MAX_BEFORE_RESET 300 // max amount you want to sell in the atm before having to reset power
-#define DEVICE_STRING "https://XXXX.lnbits.com/fossa/api/v1/lnurl/XXXXX,XXXXXXXXXXXXXXXXXXXXXX,USD"
+#define DEVICE_STRING "https://XXXX.lnbits.com/fossa/api/v1/lnurl/XXXXX,XXXXXXXXXXXXXXXX,EUR"
+#define COIN_AMOUNTS "0.05,1.0,0.25,0.5,0.1,2.0"
+//#define BILL_AMOUNTS "0.01,0.05,0.1,0.25,0.5,1"
 
 int billAmountInt[16];
 float coinAmountFloat[6];
@@ -86,8 +96,10 @@ Adafruit_Thermal printer(&SerialPrinter);
 TFT_eSPI tft = TFT_eSPI(TFT_WIDTH, TFT_HEIGHT);
 Button BTNA(BTN1);
 
+
 void setup() {
   Serial.begin(115200);
+  Serial.println("Welcome to FOSSA, running on version: " + String(VERSION));
   Serial.println("TFT: " + String(TFT_WIDTH) + "x" + String(TFT_HEIGHT));
   Serial.println("TFT pin MISO: " + String(TFT_MISO));
   Serial.println("TFT pin CS: " + String(TFT_CS));
@@ -116,21 +128,31 @@ void setup() {
   readFiles();
   translateAll(language);
 # endif
+
   BTNA.begin();
   FlashFS.begin(FORMAT_ON_FAIL);
+
   tft.init();
+#ifdef LAYOUT_1
   tft.setRotation(1);
+#endif
   tft.invertDisplay(false);
+
+  char buf[100];
+  sprintf(buf, "TFT (%dx%d) initialized.", TFT_WIDTH, TFT_HEIGHT);
+  Serial.println(buf);
+
   printMessage("", "Loading..", "", TFT_WHITE, TFT_BLACK);
   splitSettings(deviceString);
 
 #ifdef BILL_ACCEPTOR
-  SerialBillAcceptor.begin(300, SERIAL_8N2, BILL_TX, BILL_RX);
+  SerialBillAcceptor.begin(300, SERIAL_8N2, BILL_RX, BILL_TX);
   Serial.println("Bill Acceptor serial started.");
 #endif
 #ifdef COIN_ACCEPTOR
+  pinMode(COIN_TX, INPUT);
+  pinMode(COIN_INHIBIT, INPUT);
   SerialCoinAcceptor.begin(4800, SERIAL_8N1, COIN_TX);
-  pinMode(COIN_INHIBIT, OUTPUT);
   Serial.println("Coin Acceptor serial started.");
 #endif
 #ifdef PRINTER
@@ -141,24 +163,30 @@ void setup() {
 
 void loop() {
   if (maxBeforeResetTally >= maxBeforeReset) {
+    Serial.println("Max amount reached, please reset power.");
     printMessage("", tooMuchFiatT, contactOwnerT, TFT_WHITE, TFT_BLACK);
     delay(100000000);
   } else {
+#ifdef BILL_ACCEPTOR
     SerialBillAcceptor.write(184);
-    digitalWrite(COIN_INHIBIT, HIGH);
+#endif
+#ifdef COIN_ACCEPTOR
+    digitalWrite(COIN_INHIBIT, LOW);
+#endif
     tft.fillScreen(TFT_BLACK);
     BTNA.read(); // needed to clear accidental taps
     moneyTimerFun();
-    Serial.println(total);
-    Serial.println(maxBeforeResetTally);
+    Serial.println("Total: " + String(total));
+    Serial.println("maxBeforeResetTally: " + String(maxBeforeResetTally));
     maxBeforeResetTally = maxBeforeResetTally + (total / 100);
-    Serial.println(maxBeforeResetTally);
+    Serial.println("maxBeforeResetTally: " + String(maxBeforeResetTally));
     makeLNURL();
-    qrShowCodeLNURL(scanMeT);
+    qrShowCodeLNURL();
   }
 }
 
 void moneyTimerFun() {
+  Serial.println("Waiting for money...");
   waitForTap = true;
   coins = 0;
   bills = 0;
@@ -171,10 +199,10 @@ void moneyTimerFun() {
       feedmefiat();
       feedmefiatloop();
     }
+
 #ifdef BILL_ACCEPTOR
     if (SerialBillAcceptor.available()) {
       int x = SerialBillAcceptor.read();
-
       for (int i = 0; i < billAmountSize; i++) {
         if ((i + 1) == x) {
           bills = bills + billAmountInt[i];
@@ -183,24 +211,25 @@ void moneyTimerFun() {
         }
       }
     }
-    // Turn off
-    SerialBillAcceptor.write(185);
 #endif
+
 #ifdef COIN_ACCEPTOR
     if (SerialCoinAcceptor.available()) {
       int x = SerialCoinAcceptor.read();
-        printMessage("", "WARNING: print bool", String(x), TFT_WHITE, TFT_BLACK);
-        delay(3000);
-        for (int i = 0; i < coinAmountSize; i++) {
-          if ((i + 1) == x) {
-            coins = coins + coinAmountFloat[i];
-            total = (coins + bills);
-            printMessage(coinAmountFloat[i] + currencyATM, totalT + String(total) + currencyATM, tapScreenT, TFT_WHITE, TFT_BLACK);
-          }
+      //printMessage("", "WARNING: print bool", String(x), TFT_WHITE, TFT_BLACK);
+      //delay(3000);
+      for (int i = 0; i < coinAmountSize; i++) {
+        if (i + 1 == x) {
+          coins = coins + coinAmountFloat[i];
+          total = (coins + bills);
+          String printCoin = coinAmountFloat[i] + currencyATM;
+          Serial.println("Coin inserted: " + String(i) + " -> " + printCoin);
+          String printTotal = totalT + String(total) + currencyATM;
+          Serial.println(printTotal);
+          printMessage(printCoin, printTotal, tapScreenT, TFT_WHITE, TFT_BLACK);
         }
+      }
     }
-    // Turn off
-    digitalWrite(COIN_INHIBIT, LOW);
 #endif
     BTNA.read();
     if (BTNA.wasReleased() || total > maxamount) {
@@ -209,4 +238,12 @@ void moneyTimerFun() {
     homeScreenNumColorCount++;
   }
   total = (coins + bills) * 100;
+#ifdef BILL_ACCEPTOR
+    // Turn off
+    SerialBillAcceptor.write(185);
+#endif
+#ifdef COIN_ACCEPTOR
+    // Turn off
+    digitalWrite(COIN_INHIBIT, LOW);
+#endif
 }
